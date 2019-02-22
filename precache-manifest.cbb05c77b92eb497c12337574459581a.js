b25e747f7662977add0bd46f054f813cda761292
jonathanwj <jonathanwj@users.noreply.github.com>
2019-02-22 23:34:30 +0800

    commit

---
+++ precache-manifest.cbb05c77b92eb497c12337574459581a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a03dc12cda14a16444a4",
+    "url": "/test/static/js/runtime~main.a03dc12c.js"
+  },
+  {
+    "revision": "85b4cbd663a987cc0806",
+    "url": "/test/static/js/main.85b4cbd6.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/test/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "85b4cbd663a987cc0806",
+    "url": "/test/static/css/main.c1da5e7e.chunk.css"
+  },
+  {
+    "revision": "eede984b30010ac82fa833997144f433",
+    "url": "/test/index.html"
+  }
+];
